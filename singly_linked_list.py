80dd7c8533060edfa419a04ba36e394ebad883a8
ArviRA <arvindhak47@gmail.com>
2022-08-16 21:14:17 +0530

    adding /travel

---
+++ singly_linked_list.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class Linked_list:
+    def __init__(self) -> None:
+        self.head = None
+    
+    def insert(self,data):
+        if self.head is None:
+            self.head = Node(data)
+            return
+        n = self.head
+        while n.next is not None:
+            n = n.next
+        new_node = Node(data)
+        n.next = new_node
+    
+    def travel(self):
+        n = self.head
+        while n is not None:
+            print(n.data,"-->",end=" ")
+            n = n.next
+
+LL = Linked_list()
+
+LL.insert(23)
+LL.insert(44)
+LL.travel()
