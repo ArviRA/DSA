93aff0d0c093378a2d5b7765b395c29c440133d1
ArviRA <arvindhak47@gmail.com>
2022-11-02 19:34:30 +0530

    adding in linked list

--- double_list.py
+++ double_list.py
@@ -1,3 +1,6 @@
+from tkinter import N
+
+
 class Node:
     def __init__(self,data) -> None:
         self.data = data
@@ -9,23 +12,50 @@ class Double:
         self.head = None
 
     def travel(self):
-        n = self.head
-        if n is None:
-            print("List is empty")
-            return
-        while n.next is not None:
-            print(n.data,"->")
-            n = n.next
-    def reverse_travel(self):
+        print("Normal traversal")
         n = self.head
         if n is None:
             print("List is empty")
             return
         while n is not None:
+            print(n.data,"->",end=" ")
             n = n.next
-        while n.prev is not None:
-            print(n.data)
+    def reverse_travel(self):
+        print("\nReverse traversal")
+        n = self.head
+        if n is None:
+            print("List is empty")
+            return
+        while n.next is not None:
+            n = n.next
+        while n is not None:
+            print(n.data,"->",end= " ")
             n = n.prev
+    def add_begin(self,data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        #print("Done")
+    def add_end(self,data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            n = self.head
+            while n.next is not None:
+                n = n.next
+            n.next = new_node
+            new_node.prev = n
+
 
 dl = Double()
+dl.add_begin(10)
+dl.add_begin(20)
+dl.add_end(30)
+dl.add_end(40)
 dl.travel()
+dl.reverse_travel()
