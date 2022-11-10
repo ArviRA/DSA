36825db3277cdd15b18eaadeb3ed01fe7e73299f
ArviRA <arvindhak47@gmail.com>
2022-11-10 20:22:12 +0530

    delete elements in begin/end/by value

--- double_list.py
+++ double_list.py
@@ -91,6 +91,68 @@ class Double:
                 else:
                     self.head = new_node
                 n.prev = new_node
+    def delete_begin(self):
+        if self.head == None:
+            print("List is empty")
+            return
+        if self.head.next == None:
+            self.head = None
+            print("Head deleted list is empty now")
+            return
+        self.head = self.head.next
+        self.head.prev = None
+    def delete_end(self):
+        if self.head == None:
+            print("List is empty")
+            return
+        if  self.head.next == None:
+                self.head = None
+                print("Last node is  deleted list is empty now")
+                return
+        n = self.head
+        while n.next is not None:
+            n = n.next
+        n.prev.next = None
+    def delete_value(self,x):
+        if self.head == None:
+            print("List is empty")
+            return
+        if self.head.next == None:
+            if self.head.data == x:
+                self.head = None
+                print("Head deleted list is empty now")
+                return
+        if self.head.data == x:
+                self.head = self.head.next
+                self.head.prev = None
+                return
+        n = self.head
+        while n.next is not None:
+            if n.data == x:
+                break
+            n = n.next
+        if n.next is not None:
+            n.prev.next = n.next
+            n.next.prev = n.prev
+            return
+        else:
+            if n.data == x:
+                n.prev.next = None
+                return
+        print(x,"vaue is not present")
+        
+        
+        
+        
+        
+
+        
+
+    
+
+
+
+
                 
 
 
@@ -104,9 +166,12 @@ dl.add_begin(10)
 dl.add_begin(20)
 dl.add_end(30)
 dl.add_end(40)
+#dl.add_before(100,88)
+dl.add_after(99,40)
 dl.travel()
 print("\n")
-dl.add_before(100,88)
-dl.add_after(99,40)
+#dl.delete_begin()
+dl.delete_end()
+dl.delete_value(30)
 dl.travel()
 #dl.reverse_travel()
